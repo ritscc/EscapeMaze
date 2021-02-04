6ccde6e3b45ac3ff8b1164fe2e4db549d63f5875
Hara-Yuma <ysmst35@gmail.com>
2021-02-04 17:32:12 +0900

    変更の取得

---
+++ Assets/Scripts/Utils/InputProvider/UnityInputProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils.InputProvider
+{
+    public class UnityInputProvider : IInputProvider
+    {
+        public Vector3 GetMovingDirection() => new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical")).normalized;
+
+        public Vector2 GetViewpointShift() => new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        public bool GetRunning() => Input.GetKeyDown(KeyCode.LeftShift);
+
+        public bool GetActionKey() => Input.GetKeyDown(KeyCode.E);
+
+        public bool GetUseItemKey() => Input.GetKeyDown(KeyCode.Q);
+
+        public bool GetThrowItemKey() => Input.GetKeyDown(KeyCode.T);
+
+        public bool GetLightSwitchKey() => Input.GetKeyDown(KeyCode.L);
+    }
+}
